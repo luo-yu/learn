8232c3e69acba215d478702bae77b74a4bf706f4
Judy Luo <yu.luor@gmail.com>
2015-04-11 23:54:10 -0400

    Bitwise operation

---
+++ BitManipu/BitwiseOperation.java
@@ -0,0 +1,42 @@
+public class BitwiseOperation {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Arithmetic shift
+		 * 
+		 * Bits are shifted out of either end are discarded
+		 * 
+		 * in a left arithmetic shift, zeros are shifted in 
+		 * on the right
+		 * 
+		 * in a right arithmetic shift, the sign bit (MSB) is 
+		 * shifted in on the left, thus preserving the sign of the
+		 * operand
+		 * 
+		 *  
+		 *  In java, all integer types are signed, 
+		 *  
+		 *  the << and >> performs arithmetic shift
+		 *  
+		 *  
+		 *  In a logical shift, zeros are shifted in to replace
+		 *  the discarded bits. the logical shift and the arithmetic
+		 *  left shift are exactly the same. 
+		 *  
+		 *  
+		 *  However, as the logical right shift inserts value 0 bits
+		 *  into the MSB, instead of copying the sign bit, it is ideal
+		 *  for unsigned binary numbers, while arithmetic right shift
+		 *  is ideal for signed two's complement binary numbers
+		 *  
+		 *  
+		 */
+		
+		
+		
+		
+	
+	}
+	
+}
